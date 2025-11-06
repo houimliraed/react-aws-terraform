361aa6885668e639ac72e147dbe3f3d4631db783
houimliraed <houimliraed@outlook.fr>
2025-11-06 16:00:22 +0100

    restrict the public access for the s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "react_app" {
 
 }
 
-resource "aws_s3_bucket_website_configuration" "react_app_config" {
+/* resource "aws_s3_bucket_website_configuration" "react_app_config" {
 
   bucket = aws_s3_bucket.react_app.id
   index_document {
@@ -16,19 +16,19 @@ resource "aws_s3_bucket_website_configuration" "react_app_config" {
     key = "index.html"
   }
 
-}
+} */
 
 resource "aws_s3_bucket_public_access_block" "react_app_access" {
     bucket = aws_s3_bucket.react_app.id
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
 }
 
 
 
-resource "aws_s3_bucket_policy" "react_app_policy" {
+/* resource "aws_s3_bucket_policy" "react_app_policy" {
   bucket = aws_s3_bucket.react_app.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -45,3 +45,4 @@ resource "aws_s3_bucket_policy" "react_app_policy" {
 
 }
 
+ */
