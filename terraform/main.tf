76ff0e724251bea8e4c4cd053d3a6c6aed6feed5
houimliraed <houimliraed@outlook.fr>
2025-11-06 01:21:34 +0100

    added s3 configuration and public access

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,46 @@
 
 resource "aws_s3_bucket" "react_app" {
 
-    bucket = var.bucket_name
-  
+  bucket = var.bucket_name
+
 }
+
+resource "aws_s3_bucket_website_configuration" "react_app_config" {
+
+  bucket = aws_s3_bucket.react_app.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "react_app_access" {
+    bucket = aws_s3_bucket.react_app.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+
+
+resource "aws_s3_bucket_policy" "react_app_policy" {
+  bucket = aws_s3_bucket.react_app.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws.aws_s3_bucket.react_app.arn}/*"
+      }
+    ]
+  })
+  depends_on = [ aws_s3_bucket_public_access_block.react_app_access ]
+
+}
+
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-ressources-gha"
-    key            = "github-actions/terraform.tfstate"
-    region         = "eu-west-3"
-    encrypt        = true
+    bucket  = "tf-ressources-gha"
+    key     = "github-actions/terraform.tfstate"
+    region  = "eu-west-3"
+    encrypt = true
   }
 }
 
